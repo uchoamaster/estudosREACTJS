69d0e5b3b8f6beb2a0753a6ff6a2026de9fedf2e
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-11-04 16:48:06 -0300

    criando novos componentes parte 2

--- curso-react/fundamentos-react/src/components/basicos/Primeiro.js
+++ curso-react/fundamentos-react/src/components/basicos/Primeiro.js
@@ -1,3 +1,11 @@
+import React from 'react'
+
 export default function Primeiro() {
-    return 'Primeiro Componente'
+    const msg = 'Seja bem vindo(a)!'
+    return (
+    <div>
+        <h2>Primeiro Componente</h2>
+        <p>{msg}</p>
+    </div>
+    )
 }
