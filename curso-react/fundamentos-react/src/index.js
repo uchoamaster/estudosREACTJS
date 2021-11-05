377cfe099ff0cc1c836054ead1338bdea7db2de3
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-11-05 15:09:05 -0300

    react fragment

--- curso-react/fundamentos-react/src/index.js
+++ curso-react/fundamentos-react/src/index.js
@@ -5,11 +5,12 @@ import React from 'react';
 
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 
 
 reactDOM.render(
-    <div>
+    <div id="app">
         {/* pode-se fechar o componente como se abre e fecha tags com o nome dele ou apenas com a tag de fechamento */}
         <Primeiro></Primeiro> 
         <ComParametro 
@@ -18,6 +19,7 @@ reactDOM.render(
         <ComParametro 
         titulo="Situação do Aluno" 
         aluno="Maria" nota={3.9} />
+        <Fragmento />
     </div>,
 document.getElementById('root')
 );
