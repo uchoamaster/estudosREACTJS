7868b0d2c8ed8af5a06b36b0d70da9c3f19aafff
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-11-05 16:36:14 -0300

    desafio numero aleatorio

--- curso-react/fundamentos-react/src/App.jsx
+++ curso-react/fundamentos-react/src/App.jsx
@@ -4,11 +4,13 @@ import React from 'react'
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
+import Aleatorio from './components/basicos/Aleatorio'
 
 
 export default () =>
     <div id="app">
         <h1>Fundamentos React</h1>
+        <Aleatorio />
         <Fragmento />
         <ComParametro
             titulo="Situação do Aluno"
---
+++ curso-react/fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Aleatorio(props) {
+    return (
+        <>
+            <h2>Aleatorio</h2>
+            <p>Atualize para gerar um número aleatório novo</p>
+            <p>O número aleatório Mínimo</p>
+        </>
+    )
+}
