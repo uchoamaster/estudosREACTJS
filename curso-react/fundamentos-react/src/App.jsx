62e2695a2567f0729d7e219178fa6a36a5336b1d
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-11-12 15:45:16 -0300

    componentes filhos parte 2 final

--- curso-react/fundamentos-react/src/App.jsx
+++ curso-react/fundamentos-react/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import Card from './components/layout/Card'
 import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Aleatorio from './components/basicos/Aleatorio';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
@@ -14,20 +15,29 @@ export default () =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+
         <Card titulo="#05 - Componente com Filhos" color="#00c8f8">
-            <Familia sobrenome="Ferreira" />
+            <Familia sobrenome="Silva">
+            <FamiliaMembro nome="Pedro" />
+            <FamiliaMembro nome="Ana" />
+            <FamiliaMembro nome="Gustavo"/> 
+            </Familia>
         </Card>
+
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
             <Aleatorio min={1} max={60}/>
             </Card>
+
             <Card titulo="#03 - Fragmento" color="#e94c6f">
             <Fragmento />
             </Card>
+
             <Card titulo="#02 - Com Parâmetro" color="#e8b71a">
             <ComParametro
                 titulo="Situação do Aluno"
                 aluno="Carlos" nota={9.3} />
             </Card>
+
             <Card titulo="#01 - Primeiro Componente" color="#588c73">
             {/* pode-se fechar o componente como se abre e fecha tags com o nome dele ou apenas com a tag de fechamento */}
             <Primeiro></Primeiro>
--- curso-react/fundamentos-react/src/components/basicos/Familia.jsx
+++ curso-react/fundamentos-react/src/components/basicos/Familia.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import FamiliaMembro from './FamiliaMembro';
+import React, { cloneElement } from 'react';
+// import FamiliaMembro from './FamiliaMembro';
 
 export default props => {
     return  (
         <div>
-            <FamiliaMembro nome="Pedro" sobrenome={props.sobrenome}/>
-            <FamiliaMembro nome="Ana" {...props}/>  {/* spread operator*/ }
-            <FamiliaMembro nome="Gustavo" sobrenome="Silva"/>
+            {props.children.map((child, i) => {
+                    return cloneElement(child, { ...props, key: i})
+                })}
+            
         </div>
     )
 }
